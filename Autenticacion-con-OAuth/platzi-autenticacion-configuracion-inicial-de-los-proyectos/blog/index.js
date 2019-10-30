f3370492b0fea1945669f6a9842e45d85455d4cb
diassor <gruasedwin@gmail.com>
2019-10-29 22:15:58 -0400

    Fix getUserInfo

--- Autenticacion-con-OAuth/platzi-autenticacion-configuracion-inicial-de-los-proyectos/blog/index.js
+++ Autenticacion-con-OAuth/platzi-autenticacion-configuracion-inicial-de-los-proyectos/blog/index.js
@@ -35,6 +35,50 @@ app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+function getUserInfo(accessToken) {
+  if (!accessToken) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: "https://api.spotify.com/v1/me",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
+
+function getUserPlaylists(accessToken, userId) {
+  if (!accessToken || !userId) {
+    return Promise.resolve(null);
+  }
+
+  const options = {
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    headers: { Authorization: `Bearer ${accessToken}` },
+    json: true
+  };
+
+  return new Promise((resolve, reject) => {
+    request.get(options, function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        reject(error);
+      }
+
+      resolve(body);
+    });
+  });
+}
+
 // routes
 app.get("/", async function(req, res, next) {
   const { access_token: accessToken } = req.cookies;
